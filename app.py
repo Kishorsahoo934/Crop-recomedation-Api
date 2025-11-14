35f59c720a51a394c40a9f0e041cc1d2d18c7b3a
Kishor  kumar Sahoo <kishorsahoo7894@gmail.com>
2025-11-14 21:11:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -306,4 +306,6 @@ async def predict_disease_api(file: UploadFile = File(...)):
 # ✅ RUN SERVER
 # ======================================================
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
